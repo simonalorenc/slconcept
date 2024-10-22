d0918ae0106b1d50ca20f94ec827f0a01babe854
Simona Lorenc <lorenc.simona@gmail.com>
2024-10-22 12:59:27 +0200

    about-me

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 import { PortfolioPreviewComponent } from '../portfolio-preview/portfolio-preview.component';
 import { OfferComponent } from '../offer/offer.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HomeComponent, PortfolioPreviewComponent, OfferComponent],
+  imports: [HomeComponent, PortfolioPreviewComponent, OfferComponent, AboutMeComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })

10effce48b9071435cc8d8de0ab9c4187cb0fafc
Simona Lorenc <lorenc.simona@gmail.com>
2025-01-06 21:54:17 +0100

    Lazy load home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

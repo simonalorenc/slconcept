f60f96fd2614abd221f3659220427e2618745e64
Simona Lorenc <lorenc.simona@gmail.com>
2025-01-06 15:58:14 +0100

    add page not fund component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { AppRoutes } from './app-routes.enum';
-import { HomeComponent } from './home/home.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { OfferComponent } from './offer/offer.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { PortfolioDetailComponent } from './portfolio-detail/portfolio-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: AppRoutes.Main, component: MainPageComponent },
@@ -21,5 +21,5 @@ export const routes: Routes = [
     component: PortfolioDetailComponent,
   },
   { path: AppRoutes.AboutMe, component: AboutMeComponent },
-  // { path: '**', component: PageNotFoundComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];

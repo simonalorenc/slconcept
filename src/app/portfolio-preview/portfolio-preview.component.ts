7d30be570a68d7914366a2a760f2e5f65fd12de9
Simona Lorenc <lorenc.simona@gmail.com>
2025-01-07 19:27:39 +0100

    Add lazy portfolio image

--- src/app/portfolio-preview/portfolio-preview.component.ts
+++ src/app/portfolio-preview/portfolio-preview.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AppRoutes } from '../app-routes.enum';
 import { RouterModule } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio-preview',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgOptimizedImage],
   templateUrl: './portfolio-preview.component.html',
   styleUrl: './portfolio-preview.component.scss'
 })
